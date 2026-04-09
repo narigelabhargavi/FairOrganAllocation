45b51fe357a812517b45f1cf736da1a4290bf949
Malreddy-Manogna <manognareddy2006@gmail.com>
2026-04-09 18:22:22 +0530

    Updated CORS

--- organ-allocation-system/src/main/java/com/organ/allocation/controller/PatientController.java
+++ organ-allocation-system/src/main/java/com/organ/allocation/controller/PatientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/patients")
 public class PatientController {
 
